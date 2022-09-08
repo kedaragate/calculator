ea2b8c2d2b4bb8115181e7e5fa7768b2dd83e701
kedaragate <100003176+kedaragate@users.noreply.github.com>
2022-09-08 10:13:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let deleteButton = document.querySelector(".delete-button");
 let equalButton = document.querySelector(".equal-button");
 let previousOperand = document.querySelector(".previous-operand");
 let currentOperand = document.querySelector(".current-operand");
+let operator;
 
 numberButton.forEach((button) => {
   button.addEventListener("click", (e) => {
@@ -15,13 +16,17 @@ numberButton.forEach((button) => {
 
 operationButton.forEach((button) => {
   button.addEventListener("click", (e) => {
-    let operator = e.target.innerText;
+    operator = e.target.innerText;
     previousOperand.textContent = currentOperand.textContent;
-    if (operator === "+") {
+currentOperand.textContent ="";
+    
+  });
+});
+equalButton.addEventListener("click",()=>{
+if (operator === "+") {
       return (
         parseFloat(previousOperand.textContent) +
         parseFloat(currentOperand.textContent)
       );
     }
-  });
-});
+})
