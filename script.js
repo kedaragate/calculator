5603876779285af2b3f69455e43fed978145b45d
Kedar Agate <kedar.agate@gmail.com>
2022-09-09 08:22:16 +0530

    m

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ let deleteButton = document.querySelector(".delete-button");
 let equalButton = document.querySelector(".equal-button");
 let previousOperand = document.querySelector(".previous-operand");
 let currentOperand = document.querySelector(".current-operand");
+let memoryHideButton = document.querySelector(".memory-hide-button");
+let memoryShowButton = document.querySelector(".memory-show-button");
 let operator;
+let memoryArray = [];
 
 allClearButton.addEventListener("click", () => {
   currentOperand.textContent = "";
@@ -24,7 +27,7 @@ numberButton.forEach((button) => {
     currentOperand.textContent =
       currentOperand.textContent + e.target.innerText;
     if (currentOperand.textContent.startsWith(".")) {
-      currentOperand.textContent = "0.";
+      currentOperand.textContent = "0. ";
     }
   });
 });
@@ -32,12 +35,16 @@ numberButton.forEach((button) => {
 operationButton.forEach((button) => {
   button.addEventListener("click", (e) => {
     operator = e.target.innerText;
-    previousOperand.textContent =
-      previousOperand.textContent + currentOperand.textContent + operator;
+
+    previousOperand.textContent = currentOperand.textContent + operator;
     currentOperand.textContent = "";
   });
 });
 equalButton.addEventListener("click", () => {
+  if (currentOperand.textContent == NaN) {
+    return;
+  }
+  memoryArray.push(previousOperand.textContent);
   if (operator == "+") {
     currentOperand.textContent =
       parseFloat(previousOperand.textContent) +
@@ -52,7 +59,10 @@ equalButton.addEventListener("click", () => {
       parseFloat(currentOperand.textContent);
   } else if (operator == "/") {
     currentOperand.textContent =
-      parseFloat(previousOperand.textContent) /
-      parseFloat(currentOperand.textContent);
+      (parseFloat(previousOperand.textContent) * 10) /
+      (parseFloat(currentOperand.textContent) * 10);
   }
+});
+memoryShowButton.addEventListener("click", () => {
+  currentOperand.textContent = memoryArray.at(-1);
 });
